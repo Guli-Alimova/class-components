23bc21a865e08e0dbe66aa4e2c2804c3f183156f
Guli-Alimova <86463317+Guli-Alimova@users.noreply.github.com>
2024-07-08 03:34:58 +0500

    fix:product item type

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -19,19 +19,24 @@ class Cards extends Component<Props> {
             <div>Not found</div>
           ) : (
             data?.products.map(
-              (product: {
-                id: number
-                title: string
-                category: string
-                images: []
-                price: number
-              }) => (
+              (product
+                // id: number
+                // title: string
+                // category: string
+                // images: []
+                // price: number
+              ) => (
                 <div className='item' key={product.id}>
                   <p>Product Name: {product.title}</p>
                   <span>Category: {product.category}</span>
                   <div>Price: {product.price}</div>
                   <div className='product-image'>
-                    <img src={product.images[0]} alt={product.title} />
+                  {
+                      product.images.map(item => (
+<img src={item} alt={product.title} />
+                    ))
+                    
+            }
                   </div>
                 </div>
               ),
--- src/types.ts
+++ src/types.ts
@@ -12,3 +12,5 @@ export type Products = {
   images: []
   price: number
 }
+
+  
